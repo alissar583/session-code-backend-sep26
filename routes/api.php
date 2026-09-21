274ee5eb955a73575b591c9017bf50899c72ab10
Mahmoud Trro <mahmoudtrro@gmail.com>
2026-09-21 19:34:51 +0300

    crud users

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('users')->group(function () {
 
-    Route::post('/',[UserController::class,'store']);
+    Route::post('/', [UserController::class, 'store']);
 
     Route::get('/', [UserController::class, 'index']);
 
@@ -23,4 +23,48 @@ Route::prefix('users')->group(function () {
         DB::table('users')
             ->where('id', $id)->delete();
     });
+});
+
+
+
+
+
+Route::get('users', function () {
+    return DB::table('users')->get();
+});
+
+
+
+
+
+Route::post('users', function () {
+    DB::table("users")->insert([
+        [
+            "name" => "test",
+            "email" => "test",
+            "password" => "test"
+        ]
+    ]);
+});
+
+
+
+
+Route::put('users/{id}', function ($id) {
+    DB::table('users')
+        ->where('id', $id)
+        ->update([
+            'name' => "saf"
+        ]);
+});
+
+
+Route::get('users', function () {
+    DB::table('users')
+    ->where('id',1)
+    ->insert([
+        [
+
+        ]
+    ]);
 });
